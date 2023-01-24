c8fc886afa7995fb8bf41bd0992b572b6791d49e
Ekaterina Baranova <katerina.25091999@gmail.com>
2023-01-24 17:54:32 +0500

    Made the setup of the Head and Document components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>MyTop - наш лучший топ</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
