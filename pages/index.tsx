7b652d7f256df4880199e85b1e448d2d60348124
Ekaterina Baranova <katerina.25091999@gmail.com>
2023-02-03 18:39:28 +0500

    Hooks Usage Rules and Rating Component-1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { Button, Htag, P, Tag } from '@/components';
+import React, { useState } from "react";
+import { Button, Htag, P, Rating, Tag } from '@/components';
 
 export default function Home(): JSX.Element {
+	const [counter, setCounter] = useState<number>(0);
 	return (
 		<>
-			<Htag tag='h1'>Текст</Htag>
-			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Htag tag='h1'>{counter}</Htag>
+			<Button appearance='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
 			<Button appearance='ghost' arrow='down'>Кнопка</Button>
 			<P>Студенты освоят не только hard skills, необходимые для работы веб-дизайнером, но и soft skills — навыки, которые позволят эффективно взаимодействовать в команде с менеджерами, разработчиками и маркетологами. Выпускники факультета могут успешно конкурировать с веб-дизайнерами уровня middle.</P>
 			<P size='small'>Напишу сразу в двух курсах, так как проходил оба. Java будет многим непросвещённым сложновата в изучении, но здесь перевес из-за лидирующего положения языка как самого популярного в программировании. Выбор мой пал на эту профессию еще и потому, что Java-разработчики получают самую большую зарплату. Хотя Python начинает догонять Java по многим моментам, но вот в крупном екоме разработке Джава все-таки остается главенствующей сейчас. Скажу так – полнота программы и интенсивность присуща обоим курсам GeekBrains. Хочу отметить, что с первого дня занятий вы приступаете к практике и получаете опыт коммерческой разработки уже в свое резюме. Скажу вам как прошедший это – реально помогло в трудоустройстве!</P>
@@ -14,6 +15,7 @@ export default function Home(): JSX.Element {
 			<Tag size='middle' color='red'>Red</Tag>
 			<Tag size='small' color='green'>green</Tag>
 			<Tag color='primary'>green</Tag>
+			<Rating rating={4} />
 		</>
 	);
 }
