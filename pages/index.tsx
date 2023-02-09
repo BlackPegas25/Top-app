66f5410cc6efe1c01a58bf87978b84cc033e4efe
Ekaterina Baranova <katerina.25091999@gmail.com>
2023-02-09 18:05:54 +0500

    useContext

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,10 +21,6 @@ function Home({ menu }: HomeProps): JSX.Element {
 			<Tag size='small' color='green'>green</Tag>
 			<Tag color='primary'>green</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
-			<ul>
-				{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
-			</ul>
-
 		</>
 	);
 }
