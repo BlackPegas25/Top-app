ed671f71f00f7af02fcfe42a93572f672bca6f54
Ekaterina Baranova <katerina.25091999@gmail.com>
2023-02-13 16:45:37 +0500

    Routing structure

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -3,20 +3,9 @@ import cn from 'classnames';
 import { useContext} from 'react';
 import { AppContext } from '@/context/app.context';
 import { FirstLevelMenuItem, PageItem } from '@/interfaces/menu.interface';
-import BooksIcon from './icons/books.svg';
-import CoursesIcon from './icons/courses.svg';
-import ProductsIcon from './icons/products.svg';
-import ServicesIcon from './icons/services.svg';
-import { TopLevelCategory } from '@/interfaces/page.interface';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon/>, id: TopLevelCategory.Courses },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon/>, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon/>, id: TopLevelCategory.Products },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon/>, id: TopLevelCategory.Services }
-];
+import { firstLevelMenu } from '@/helpers/helpers';
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
